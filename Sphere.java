a5027a194b0e92541fb9ed189a7d6fd98b35971a
gabrielshufelt <131308994+gabrielshufelt@users.noreply.github.com>
2023-04-19 17:53:17 -0400

    Update Sphere.java

--- Sphere.java
+++ Sphere.java
@@ -1,11 +1,15 @@
 package raytracer;
 
 public class Sphere {
+	// sphere takes in a center point c, and a radius r
 	public Vector c;
 	public double r;
 	public Sphere(Vector c, double r) {this.c = c;this.r = r;}
 	public Vector getNormal(Vector p) {return (p.sub(c)).div(r);}
+	// intersects function returns true if this sphere intersects with the ray passed in
 	public boolean intersects(Ray ray) {
+		// if the discriminant of the intersection formula is a negative 
+		// or very small number, it does not intersect.
 		Vector p = ray.o.sub(c);
 		double b = 2*p.dot(ray.d);
 		double c = p.dot(p)-(r*r);
@@ -18,6 +22,7 @@ public class Sphere {
 		double b = 2*p.dot(ray.d);
 		double c = p.dot(p)-(r*r);
 		double disc = Math.sqrt(b*b - 4*c);
+		// solution point is the smaller point
 		double t0 = -b-disc;
 		double t1 = -b+disc;
 		if (t0 < t1) {return t0;}
